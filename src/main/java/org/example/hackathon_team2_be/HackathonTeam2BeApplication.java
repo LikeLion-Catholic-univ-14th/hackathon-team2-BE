24c94a0ef48f072ba3509d0524e46f01059558be
wonmin3180 <wonmin3180@gmail.com>
2026-08-05 23:02:38 +0900

    initial commit

---
+++ src/main/java/org/example/hackathon_team2_be/HackathonTeam2BeApplication.java
@@ -0,0 +1,13 @@
+package org.example.hackathon_team2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTeam2BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonTeam2BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hackathon_team2_be/HackathonTeam2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackathon_team2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonTeam2BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
