76d3afc17940eefad8b1874585c187c34fbd5262
syeoooun98 <syeoooun98@catholic.ac.kr>
2026-08-18 00:40:52 +0900

    main() 실행 전 dotenv 설정

--- src/main/java/org/example/hackathon_team2_be/HackathonTeam2BeApplication.java
+++ src/main/java/org/example/hackathon_team2_be/HackathonTeam2BeApplication.java
@@ -1,5 +1,6 @@
 package org.example.hackathon_team2_be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HackathonTeam2BeApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(HackathonTeam2BeApplication.class, args);
     }
-
 }
